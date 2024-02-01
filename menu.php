1cb51a19bc298d87f685a934d67bf621a3be6892
Yonathhh <yonathanlilis@gmail.com>
2024-02-02 05:38:58 +0700

    menu

--- menu.php
+++ menu.php
@@ -1,33 +1,48 @@
 <?php
-    session_start();
+session_start();
 
-    if (!array_key_exists("username", $_SESSION)) {
-        header("location:logout.php");
-    }
+if (!array_key_exists("username", $_SESSION)) {
+    header("location:logout.php");
+}
 ?>
 
-<nav>
-    <ul>
-        <li><a href="home.php">HOME</a></li>
-        <li>MASTER
-            <ul>
-                <?php if ($_SESSION["level"] == "admin") : ?>
-                    <li><a href="user.php">User</a></li>
-                <?php endif ?>
-                <li><a href="barang.php">Barang</a></li>
-            </ul>
-        </li>
-        <li>TRANSAKSI
-            <ul>
-                <li><a href="penjualan.php">Penjualan</a></li>
-                <li><a href="pembelian.php">Pembelian</a></li>
-            </ul>
-        </li>
-        <li>Selamat datang, <?= $_SESSION["username"] ?>!
-            <ul>
-                <li><a href="profil.php">Profil</a></li>
-                <li><a href="logout.php">Log out</a></li>
-            </ul>
-        </li>
-    </ul>
-</nav>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title</title>
+    <link rel="stylesheet" href="menu.css">
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a href="home.php">HOME</a></li>
+            <li>MASTER
+                <ul>
+                    <?php if ($_SESSION["level"] == "admin") : ?>
+                        <li><a href="user.php">User</a></li>
+                    <?php endif ?>
+                    <li><a href="barang.php">Barang</a></li>
+                </ul>
+            </li>
+            <li>TRANSAKSI
+                <ul>
+                    <li><a href="penjualan.php">Penjualan</a></li>
+                    <li><a href="pembelian.php">Pembelian</a></li>
+                </ul>
+            </li>
+            <li class="user-profile">
+                Selamat datang, <?= $_SESSION["username"] ?>!
+                <ul>
+                    <li><a href="profil.php">Profil</a></li>
+                    <li><a href="logout.php">Log out</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</body>
+
+</html>
