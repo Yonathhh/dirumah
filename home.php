20751c0d36a956a52e1cbc39fdab8344351125f9
Yonathhh <yonathanlilis@gmail.com>
2024-03-23 11:22:23 +0700

    home

---
+++ home.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>HOME</title>
+    <link rel="stylesheet" href="css/home.css">
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- <link rel="icon" type="image/x-icon" href="assets/favicon.ico" /> -->
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900"
+        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- AOS -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+</head>
+
+<body>
+    <div class="background-home"></div>
+    <?php include "menu.php"; ?>
+    <!-- baru boostrap -->
+    <section id="scroll">
+        <div class="container px-5">
+            <div class="row gx-5 align-items-center">
+                <div class="col-lg-6 order-lg-2">
+                    <div class="p-5" data-aos="fade-left"><img class="img-fluid rounded-circle"
+                            src="images/hp-canggih-1.jpg" alt="..." /></div>
+                </div>
+                <div class="col-lg-6 order-lg-1" data-aos="fade-left">
+                    <div class="p-5">
+                        <h2 class="display-4">Melangkah ke Masa Depan</h2>
+                        <p>Temukan Inovasi Terbaru yang Merubah Cara Anda Berinteraksi dengan Teknologi yang Semakin Canggih, Membawa Kemudahan dan Efisiensi dalam Kehidupan Sehari-hari Anda.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Content section 2-->
+    <section>
+        <div class="container px-5">
+            <div class="row gx-5 align-items-center">
+                <div class="col-lg-6">
+                    <div class="p-5" data-aos="fade-right"><img class="img-fluid rounded-circle"
+                            src="images/hp-canggih-2.jpg" alt="..." /></div>
+                </div>
+                <div class="col-lg-6" data-aos="fade-right">
+                    <div class="p-5">
+                        <h2 class="display-4">Teknologi Masa Depan</h2>
+                        <p>Jelajahi Ragam Perangkat Elektronik Canggih yang Mewujudkan Impian Digital Anda, Menyatu dalam Era Digitalisasi yang Mengubah Cara Anda Beraktivitas.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Content section 3-->
+    <section>
+        <div class="container px-5">
+            <div class="row gx-5 align-items-center">
+                <div class="col-lg-6 order-lg-2">
+                    <div class="p-5" data-aos="fade-left"><img class="img-fluid rounded-circle"
+                            src="images/hp-canggih-3.jpg" alt="..." /></div>
+                </div>
+                <div class="col-lg-6 order-lg-1" data-aos="fade-left">
+                    <div class="p-5">
+                        <h2 class="display-4">Menuju Era Elektronik Revolusioner</h2>
+                        <p>Temukan Alat Canggih yang Membawa Anda ke Zaman Baru dalam Keterhubungan yang Tak Terbatas, Mewujudkan Visi Masa Depan yang Terhubung</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="bg-gradient-primary-to-secondary">
+        <div class="about-container" data-aos="zoom-in">
+            <h2>Tentang Kami</h2>
+            <p>Di GlowTech, kami memahami betapa pentingnya teknologi dalam mendukung aktivitas bisnis, oleh karena itu
+                kami
+                menawarkan rangkaian lengkap alat elektronik terkemuka, mulai dari handphone, laptop, dan berbagai
+                perangkat teknologi lainnya. Kami selalu menghadirkan produk-produk berkualitas terbaik dengan harga
+                yang
+                kompetitif, sehingga Anda dapat menjalankan bisnis Anda dengan efisien dan sukses.
+            </p>
+        </div>
+    </div>
+
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+</body>
+
+</html>
---
+++ user.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>User</title>
+    <link rel="stylesheet" href="css/user.css">
+</head>
+
+<body>
+    <?php include "menu.php"; ?>
+
+    <!-- Div untuk menampilkan notifikasi -->
+    <div id="notification">
+        <?php
+        if (isset ($_SESSION["notification"])) {
+            echo $_SESSION["notification"];
+            unset($_SESSION["notification"]); // Hapus notifikasi setelah ditampilkan
+        }
+        ?>
+    </div>
+
+    <?php
+    if ($_SESSION["level"] != "admin") {
+        echo "<script>
+            alert('Anda tidak dapat mengakses halaman ini');
+            window.location.href = 'home.php'; // Redirect ke halaman home.php
+          </script>";
+        exit;
+    }
+
+    require "koneksi.php";
+
+    $sql = "SELECT * FROM user";
+    $query = mysqli_query($koneksi, $sql);
+    ?>
+
+    <div class="container">
+        <h1>Data User</h1>
+        <div class="data-box">
+            <!-- Tombol Tambah diletakkan di luar card-container -->
+            <form action="new-user.php" method="GET">
+                <button type="submit" class="button-tambah">Tambah</button>
+            </form>
+            <div class="card-container"> <!-- Container untuk kartu -->
+                <?php while ($user = mysqli_fetch_array($query)): ?>
+                    <div class="card"> <!-- Setiap entri sebagai kartu -->
+                        <div class="card-details">
+                            <h3>
+                                <?= $user["username"] ?>
+                            </h3>
+                            <?php if (isset ($user['photo']) && !empty ($user['photo'])): ?>
+                                <img src="<?= $user['photo'] ?>" alt="" id="profile-img" title="Ganti Profil">
+                            <?php else: ?>
+                                <img src="images/empty-profil.webp" alt="" id="profile-img">
+                            <?php endif; ?>
+                            <p>Level:
+                                <?= $user["level"] ?>
+                            </p>
+                            <p>Dibuat pada:
+                                <?= $user["created_at"] ?>
+                            </p>
+                            <p>Diubah pada:
+                                <?= $user["updated_at"] ?>
+                            </p>
+                            <div class="button-container">
+                                <form action="read-user.php" method="GET">
+                                    <input type="hidden" name="id" value='<?= $user["id"] ?>'>
+                                    <button type="submit" class="button-lihat">Lihat</button>
+                                </form>
+                                <form action="delete-user.php" method="POST" onsubmit="return konfirmasi(this)">
+                                    <input type="hidden" name="id" value='<?= $user["id"] ?>'>
+                                    <button type="submit" class="button-delete">Hapus</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile ?>
+            </div>
+        </div>
+    </div>
+    <script>
+        function konfirmasi(form) {
+            formData = new FormData(form);
+            id = formData.get("id");
+            return confirm(`Hapus user '${id}'?`);
+        }
+    </script>
+</body>
+
+</html>
